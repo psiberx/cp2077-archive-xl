3fc2c5adb67bd8bbbcd52484079735a1d08857ef
pv <p@siberx.dev>
2024-05-07 03:35:35 +0300

    Refactoring

--- tools/dist/steps/compose-bundle.ps1
+++ tools/dist/steps/compose-bundle.ps1
@@ -1,6 +1,6 @@
 param ($StageDir, $ReleaseBin, $ProjectName)
 
-$DataDir = "${StageDir}/red4ext/plugins/${ProjectName}/Data"
+$DataDir = "${StageDir}/red4ext/plugins/${ProjectName}/Bundle"
 
 New-Item -ItemType directory -Force -Path ${DataDir} | Out-Null
 Copy-Item -Path "data/*" -Destination ${DataDir}
