15e80495e09fcceeb0f5ec7f57a19fdf41bd19c2
psiberx <psiberx@gmail.com>
2024-11-26 18:25:20 +0300

    Skip world widget limit check

--- src/App/Patches/WorldWidgetLimitPatch.cpp
+++ src/App/Patches/WorldWidgetLimitPatch.cpp
@@ -3,7 +3,7 @@
 void App::WorldWidgetLimitPatch::OnBootstrap()
 {
     auto& code = Core::RawPtr<Red::AddressLib::InkWorldLayer_UpdateComponents, uint8_t[0x100]>::Ref();
-    uint8_t op[] = {0x83, 0xFB, 0x0A};
+    uint8_t op[] = {0x83, 0xFB, 0x0A, 0x73, 0x16};
     bool success = false;
 
     DWORD oldProtect = 0;
@@ -12,7 +12,8 @@ void App::WorldWidgetLimitPatch::OnBootstrap()
     auto it = std::search(std::begin(code), std::end(code), std::begin(op), std::end(op));
     if (it != std::end(code))
     {
-        *(it + 2) = 0xFF;
+        *(it + 3) = 0x90;
+        *(it + 4) = 0x90;
         success = true;
     }
 
