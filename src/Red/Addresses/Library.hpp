af3ec45253b5aa99251796256154b98eddbd178d
psiberx <psiberx@gmail.com>
2024-12-11 10:04:25 +0300

    Update addresses for patch 2.2

--- src/Red/Addresses/Library.hpp
+++ src/Red/Addresses/Library.hpp
@@ -11,8 +11,8 @@ constexpr uint32_t AnimatedComponent_InitializeAnimations = 2855474741;
 
 constexpr uint32_t AppearanceChanger_ComputePlayerGarment = 3243419919;
 constexpr uint32_t AppearanceChanger_GetBaseMeshOffset = 4219677283;
-constexpr uint32_t AppearanceChanger_GetSuffixes = 63057648;
-constexpr uint32_t AppearanceChanger_GetSuffixValue = 1003499294;
+constexpr uint32_t AppearanceChanger_GetSuffixes = 332345115;
+constexpr uint32_t AppearanceChanger_GetSuffixValue = 1317875529;
 constexpr uint32_t AppearanceChanger_RegisterPart = 3169139695;
 constexpr uint32_t AppearanceChanger_SelectAppearanceName = 2770550105;
 
@@ -168,7 +168,7 @@ constexpr uint32_t TPPRepresentationComponent_OnAttach = 4129169021;
 constexpr uint32_t TPPRepresentationComponent_OnItemEquipped = 4010810747;
 constexpr uint32_t TPPRepresentationComponent_OnItemUnequipped = 1933319146;
 constexpr uint32_t TPPRepresentationComponent_RegisterAffectedItem = 3037343626;
-constexpr uint32_t TPPRepresentationComponent_IsAffectedSlot = 678894266;
+constexpr uint32_t TPPRepresentationComponent_IsAffectedSlot = 684464829;
 
 constexpr uint32_t TweakDB_Load = 3602585178; // game::data::TweakDB::LoadOptimized
 }
