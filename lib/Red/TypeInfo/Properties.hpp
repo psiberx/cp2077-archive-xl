949e0aa446d8b5fa0e770f439347237c6508a51d
psiberx <psiberx@gmail.com>
2024-12-25 23:46:37 +0300

    Update shared lib

--- lib/Red/TypeInfo/Properties.hpp
+++ lib/Red/TypeInfo/Properties.hpp
@@ -5,6 +5,13 @@
 
 namespace Red
 {
+template<typename T, typename C>
+requires (!std::is_base_of_v<ISerializable, C>)
+inline T& GetPropertyPtr(C* aContext, CName aProp)
+{
+    return GetPropertyPtr<T>(aContext, GetClass<C>(), aProp);
+}
+
 template<typename T>
 inline T* GetPropertyPtr(ISerializable* aContext, CName aProp)
 {
@@ -12,23 +19,6 @@ inline T* GetPropertyPtr(ISerializable* aContext, CName aProp)
         return nullptr;
 
     auto prop = aContext->GetType()->GetProperty(aProp);
-    if (!prop)
-        return nullptr;
-
-    return prop->GetValuePtr<T>(aContext);
-}
-
-template<typename T>
-inline T* GetPropertyPtr(void* aContext, CName aType, CName aProp)
-{
-    if (!aContext)
-        return nullptr;
-
-    auto type = GetClass(aType);
-    if (!type)
-        return nullptr;
-
-    auto prop = type->GetProperty(aProp);
     if (!prop)
         return nullptr;
 
@@ -49,19 +39,32 @@ inline T* GetPropertyPtr(void* aContext, CClass* aType, CName aProp)
 }
 
 template<typename T>
+inline T* GetPropertyPtr(void* aContext, CName aType, CName aProp)
+{
+    return GetPropertyPtr<T>(aContext, GetClass(aType), aProp);
+}
+
+template<typename T, typename C>
+requires (!std::is_base_of_v<ISerializable, C>)
+inline T& GetProperty(C* aContext, CName aProp)
+{
+    return *GetPropertyPtr<T>(aContext, GetClass<C>(), aProp);
+}
+
+template<typename T>
 inline T& GetProperty(ISerializable* aContext, CName aProp)
 {
     return *GetPropertyPtr<T>(aContext, aProp);
 }
 
 template<typename T>
-inline T& GetProperty(void* aContext, CName aType, CName aProp)
+inline T& GetProperty(void* aContext, CClass* aType, CName aProp)
 {
     return *GetPropertyPtr<T>(aContext, aType, aProp);
 }
 
 template<typename T>
-inline T& GetProperty(void* aContext, CClass* aType, CName aProp)
+inline T& GetProperty(void* aContext, CName aType, CName aProp)
 {
     return *GetPropertyPtr<T>(aContext, aType, aProp);
 }
