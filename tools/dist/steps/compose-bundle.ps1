fa9084356524f7f59fce6ea13f8ec9fbe172c329
psiberx <psiberx@gmail.com>
2025-01-19 09:52:10 +0300

    Update dist tools

--- tools/dist/steps/compose-bundle.ps1
+++ tools/dist/steps/compose-bundle.ps1
@@ -3,5 +3,5 @@ param ($StageDir, $ReleaseBin, $ProjectName)
 $BundleDir = "${StageDir}/red4ext/plugins/${ProjectName}/Bundle"
 
 New-Item -ItemType directory -Force -Path ${BundleDir} | Out-Null
-Copy-Item -Path "archive/packed/archive/pc/mod/*" -Destination ${BundleDir}
-Copy-Item -Path "archive/source/resources/*" -Destination ${BundleDir}
+Copy-Item -Path "bundle/packed/archive/pc/mod/*" -Destination ${BundleDir}
+Copy-Item -Path "bundle/source/resources/*" -Destination ${BundleDir}
